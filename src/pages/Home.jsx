3ce718db80034ecbd707db1a042475f7a5d34479
maximiliano17 <maxidossantosburgos@gmail.com>
2023-09-26 00:01:12 -0300

    Seccion noticias add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 //Components
 import Header from "../components/Header"
-import Slider from "../components/Slider"
 //Styles
 import styles from "../modules/Home.module.css"
 //Imagenes
@@ -11,11 +10,11 @@ function Home() {
     <>
       <Header />
       <div className={styles.containerHome}>
+        <div className={styles.sobreNosotros}>
+          
+        </div>
         <div className={styles.noticias}>
           <p> - Noticias - </p> 
-        <div className={styles.slider}>
-         <Slider />
-        </div>
         <div className={styles.informacion}>
           <article  className={styles.articulo}> 
             <span>Titulo</span>
