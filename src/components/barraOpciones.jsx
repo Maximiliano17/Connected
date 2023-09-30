fea100b4b5ac6211efcedc058426783c278ca641
maximiliano17 <maxidossantosburgos@gmail.com>
2023-09-29 23:11:45 -0300

    subir push acumulados

--- src/components/barraOpciones.jsx
+++ src/components/barraOpciones.jsx
@@ -6,41 +6,8 @@ function BarOptions(){
     return(
         <>
          <div className={styles.barraOp}> 
-          <section className={styles.servicios}>
-            <article className={styles.serviciosCard}>
-               <p>Pages Web</p>
-                <img src={img} alt="Foto" />
-                <ul className={styles.listaServ}>
-                    <li>Dominio</li>
-                    <li>Hosting</li>
-                    <li>Responcive Desing</li>
-                    <li>Database</li> 
-                    <li>Apk</li>
-                </ul>
-            </article>
-            <article className={styles.serviciosCard}>
-               <p>App Web</p>
-                <img src={img} alt="Foto" />
-                <ul className={styles.listaServ}>
-                    <li>Dominio</li>
-                    <li>Hosting</li>
-                    <li>Responcive Desing</li>
-                    <li>Database</li>
-                    <li>Apk</li>
-                </ul>        
-            </article>
-            <article className={styles.serviciosCard}>
-               <p>App Mobile</p>
-                <img src={img} alt="Foto" />
-                <ul className={styles.listaServ}>
-                    <li>Dominio</li>
-                    <li>Hosting</li>
-                    <li>Responcive Desing</li>
-                    <li>Database</li>
-                    <li>Apk</li>
-                </ul>
-            </article>
-          </section>
+       
+        
          </div>
         </>
     )
