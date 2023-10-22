ea28faf22fd2efb446f84be469404741eabc04f7
maximiliano17 <maxidossantosburgos@gmail.com>
2023-10-22 00:54:41 -0300

    add cambios en seccion sistemas

--- src/pages/Sistemas.jsx
+++ src/pages/Sistemas.jsx
@@ -3,24 +3,24 @@ import Header from "../components/Header"
 //Styles
 import  styles from "../modules/Sistemas.module.css"
 //Imagenes
-
+import sistemas from "../assets/sistemas/sistemasFoto.jpeg"
 function Sistemas() {
     return ( 
       <> 
       <Header />
        <div className={styles.sistemasContent}> 
-          <div className={styles.imagenhome}>
-            <article className={styles.cardSistemas}>
-               <h1>Sistemas</h1>  
-               <div className={styles.content}>
-                <p>En Connected World, ofrecemos sistemas pre creados para cubrir diversas necesidades empresariales. Listos para implementarse de inmediato, son ideales para empresas que buscan soluciones eficientes y probadas. Desde gestión empresarial hasta plataformas de comercio electrónico, nuestra colección abarca diversas áreas, proporcionando acceso rápido a soluciones tecnológicas de vanguardia. Simplificamos la adopción tecnológica, permitiendo a las empresas optimizar operaciones y mejorar rendimiento con soluciones confiables.</p>
-               </div>
-            </article> 
-          </div>
+         <img src={sistemas} alt="Foto Sistemas" />
+         <section className={styles.infoSis}>
+            <h1>System of Stock</h1>
+            <p>
+             Pañol es un proyecto que ofrece una solución completa para la gestión de herramientas en talleres, permitiendo hacer pedidos de forma rápida y eficiente, y controlando el stock de manera efectiva. Es una herramienta muy útil para mejorar la productividad en talleres y reducir el tiempo que se dedica a la gestión de herramientas.
+            </p>
+            <button>Conseguir</button>
+           </section> 
        </div> 
-      <div className={styles.sistemasDisponibles}>
-        Hola
-      </div>
+       {/* <div className={styles.sistemasDisponibles}>
+         Hola
+       </div> */}
       </>
     ) 
   }
