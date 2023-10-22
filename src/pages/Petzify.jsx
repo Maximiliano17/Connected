d9967a158401fbd7ee7232899d7c904316d7532a
maximiliano17 <maxidossantosburgos@gmail.com>
2023-10-21 21:43:50 -0300

    Nueva seccion buscador en proyecto petzify

--- src/pages/Petzify.jsx
+++ src/pages/Petzify.jsx
@@ -8,24 +8,22 @@ import petshop from "../assets/icons/petshop.png"
 import adoptar from "../assets/icons/adoptar.png"
 import perfiles from "../assets/icons/crearPerfiles.png"
 import fotos from "../assets/icons/subirFoto.png"
-import petLogo from "../assets/sistemas/petLogo.jpeg"
-//Logos Institucion
-import gato from "../assets/sistemas/gato.png"
-import cats from "../assets/sistemas/Cats.png"
-import dog from "../assets/sistemas/dogs.png"
-import eco from "../assets/sistemas/eco.jpeg"
+import section from "../assets/sistemas/newSection.png"
+import app1 from "../assets/sistemas/pet1.png"
+import app2 from "../assets/sistemas/pet2.png"
 function Petzify(){   
     return(
         <>
         <Header/>
          <div className={styles.petzifyContent}>
            <section className={styles.homePet}> 
-            <img src={petLogo} alt="Celular" />
+            <img src={app1} className={styles.imagenCelular} alt="Celular" />
+            <img src={app2} className={styles.imagenCelularRes}  alt="Celular" />
             <section className={styles.infoPetzify}>
-                <p>
+              <p>
                 <h1>Petzify</h1>
-                Petzify es una red social especializada en animales domésticos que proporciona a los dueños de mascotas un entorno dedicado a compartir sus experiencias con sus compañeros peludos. Además, permitiremos a los usuarios publicar sobre animales disponibles para adopción y solicitar donaciones para facturas médicas de sus mascotas.
-                </p>
+                 Petzify es una red social especializada en animales domésticos que proporciona a los dueños de mascotas un entorno dedicado a compartir sus experiencias con sus compañeros peludos.
+              </p>
                 <button>Donwload</button>
            </section>
            </section>
@@ -60,7 +58,7 @@ function Petzify(){
                 </article>
             </section>
          </div> 
-         <div className={styles.institucionesAdd}>
+         {/* <div className={styles.institucionesAdd}>
            <h2>Instituciones add</h2>
            <section className={styles.instituciones}>
             <img src={eco} />
@@ -68,6 +66,22 @@ function Petzify(){
             <img src={dog} />
             <img src={cats} />
            </section>
+         </div> */}
+         <div className={styles.comunidadContent}>
+          <section className={styles.tituloComunidad}>
+            <p>New Section</p>
+          </section> 
+          <div className={styles.content}>
+            <img className={styles.imagen} src={section} />
+            <img className={styles.imagenRes} src={section} />
+            <div className={styles.infoPet}>
+            <span className={styles.titulo}>Buscador de usuarios.</span>
+            <p>
+             ¡Novedad en nuestra app de red social! Introducimos una emocionante característica: el 'Buscador de Usuarios'. Ahora, encontrar y conectar con amigos y personas afines es más fácil que nunca. Simplemente utiliza esta función para buscar usuarios por nombre, intereses o ubicación. Amplía tu círculo social y descubre nuevas amistades con solo unos clics. ¡Es hora de conectar como nunca antes! ¡Prueba el buscador de usuarios hoy mismo y descubre quién está en nuestra comunidad!
+            </p>
+              <a target="_Blanck" href="https://petzify.vercel.app/">Ver mas</a>
+          </div>
+         </div>
          </div>
        </div>
         </>
